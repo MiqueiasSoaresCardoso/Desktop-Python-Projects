c39be73755282d70a6b6d69fd18b5e1279d72465
MiqueiasSoaresCardoso <miqueiasccccc@gmail.com>
2024-03-06 11:22:03 -0300

    Updating description

--- Function.py
+++ Function.py
@@ -11,22 +11,27 @@ def requestdata():
     else:
         print("Opps! Não foi possivél acessar os dados")
 
+'''
+é possivel termos valores pré-definidos nos argumentos, e caso o 
+usuário não insira nada, já teremos um valor padrão
+'''
 def calculate_average(qtdsoma = 3):
     nota = 0
     for i in range(qtdsoma):
         insercao = float(input("Insira a {} nota!>>".format(i+1)))
-        nota = nota + insercaocd
+        nota = nota + insercao
     print("O seu CAC é {}".format(nota/qtdsoma))
     if nota/qtdsoma >= 7:
         print("Candidato classificado")
     else:
         print("Candidato não classificado =(")
 
+'''Nesta função estamos trabalhando com Argumentos e o Return'''
 def percentage(numero, porcentagem):
     result = (porcentagem/100 * numero)
     return result
 
-'''
-é possivel termos valores pré-definidos noa argumentos
-, e caso o usuário não insira nada, já teremos um valor padrão
-'''
+''' O Return nos permite retornar dados de uma função, de forma que você pode armazenar
+e manipular esse dado da forma que desejar, em qualquer lugar, utilizando de acordo
+com a sua regra de negócio'''
+
