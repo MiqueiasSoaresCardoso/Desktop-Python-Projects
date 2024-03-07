a3ded0ff6b83d26cfde6c0fbd85ed8935df32c9f
MiqueiasSoaresCardoso <miqueiasccccc@gmail.com>
2024-03-07 14:26:33 -0300

    Adding saving user data via file with python

--- Drawer.py
+++ Drawer.py
@@ -11,6 +11,8 @@ def drawer():
     rand = random.randint(1,1000)
     numero = 0
     contador = 0
+    user = input("Qual é o seu nome?")
+    dados = open(f"Arquivo{user}.txt", "w")
     while numero != rand:
         contador += 1
         print("Insira o seu palpite abaixo")
@@ -20,6 +22,8 @@ def drawer():
         elif numero2 < rand:
             print("Você inseriu um numero menor =( tente novamente!")
         else:
+            dados.write(f"USER: {user} -- TENTATIVAS: {contador}")
             print("Uhuu! Você acertou, o numero era {}".format(rand))
+            dados.close()
             print("Numero de tentativas:{}".format(contador))
             continue
